0df6582118169c4c28a065b0297e1a79e4334695
curtarnold <afayedormawuli@gmail.com>
2022-09-20 14:33:39 +0000

    The last digit

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+end_gtr = "and is greater than 5"
+end_eq = "and is 0"
+end_less = "and is less than 6 and not 0"
+num_str_end = str(number)[-1]
+num = int(num_str_end)
+if num > 5:
+    print(f"Last digit of {number} is {num_str_end} {end_gtr}")
+elif num == 0:
+    print(f"Last digit of {number} is {num_str_end} {end_eq}")
+elif (num < 6) and (num != 0):
+    print(f"Last digit of {number} is {num_str_end} {end_less}")
--- 0x01-python-if_else_loops_functions/README.md
+++ 0x01-python-if_else_loops_functions/README.md
@@ -1,1 +1,2 @@
 0-positive_or_negative.py - assigns a random number to variable number each time it is called
+1-last_digit.py assigns a random signed number to the variable number each time it is executed and prints the last digit of the number stored in number
