63362ec446b69d9acb8584d04c532c317bbabb3b
curtarnold <afayedormawuli@gmail.com>
2022-09-20 14:58:22 +0000

    The last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+from ast import If
 import random
 number = random.randint(-10000, 10000)
 end_gtr = "and is greater than 5"
@@ -6,9 +7,18 @@ end_eq = "and is 0"
 end_less = "and is less than 6 and not 0"
 num_str_end = str(number)[-1]
 num = int(num_str_end)
-if num > 5:
-    print(f"Last digit of {number} is {num_str_end} {end_gtr}")
-elif num == 0:
-    print(f"Last digit of {number} is {num_str_end} {end_eq}")
-elif (num < 6) and (num != 0):
-    print(f"Last digit of {number} is {num_str_end} {end_less}")
+
+if number >= 0:
+	if num > 5:
+		print(f"Last digit of {number} is {num_str_end} {end_gtr}")
+	elif num == 0:
+		print(f"Last digit of {number} is {num_str_end} {end_eq}")
+	elif (num < 6) and (num != 0):
+		print(f"Last digit of {number} is {num_str_end} {end_less}")
+elif number == 0:
+	print(f"Last digit of {number} is {num_str_end} {end_eq}")
+elif number < 0:
+	if num != 0:
+		print(f"Last digit of {number} is -{num_str_end} {end_less}")
+	else:
+		print(f"Last digit of {number} is {num_str_end} {end_less}")
--- 0x01-python-if_else_loops_functions/README.md
+++ 0x01-python-if_else_loops_functions/README.md
@@ -1,2 +1,3 @@
 0-positive_or_negative.py - assigns a random number to variable number each time it is called
 1-last_digit.py assigns a random signed number to the variable number each time it is executed and prints the last digit of the number stored in number
+2-print_alphabet.py prints the ASCII alphabet in lowercase, not followed by a new line
