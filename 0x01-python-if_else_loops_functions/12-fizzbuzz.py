6997f94b1c58fdf3643271c8c379f5a9968f792e
curtarnold <afayedormawuli@gmail.com>
2022-09-21 00:47:18 +0000

    combine

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -5,9 +5,11 @@ BUZZ = "Buzz"
 
 def fizzbuzz():
     for number in range(1, 101):
-        if (number % 3 == 0):
-            print("%s" % (FIZZ), end=' ')
+        if (number % 3 == 0 and number % 5 == 0):
+            print('{}{}'.format(FIZZ, BUZZ), end=' ')
+        elif (number % 3 == 0):
+            print('{}'.format(FIZZ), end=' ')
         elif (number % 5 == 0):
-            print("%s" % (BUZZ), end=' ')
+            print('{}'.format(BUZZ), end=' ')
         else:
-            print("%d" % (number), end=' ')
+            print('{}'.format(number), end=' ')
