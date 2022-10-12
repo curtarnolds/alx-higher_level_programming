dad41e89370058e891c19c33d04d550f0e637d0a
curtarnold <afayedormawuli@gmail.com>
2022-10-12 14:14:34 +0000

    combine

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
+
 def safe_print_list_integers(my_list=[], x=0):
-    '''Prints the first x elements of a list and only integers'''
-    count = 0
-    for i in range(x):
+    ret = 0
+    for i in range(0, x):
         try:
-            print('{:d}'.format(my_list[i]), end='')
-            count += 1
-        except (TypeError, ValueError):
+            print("{:d}".format(my_list[i]), end="")
+            ret += 1
+        except (ValueError, TypeError):
             continue
-    print('')
-    return count
+    print("")
+    return (ret)
--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 
+
 def safe_print_division(a, b):
-    '''Divides 2 integers and prints the result'''
-    result = 0
     try:
-        result = a / b
-    except ZeroDivisionError:
-        result = None
+        div = a / b
+    except (TypeError, ZeroDivisionError):
+        div = None
     finally:
-        print('Inside result: {}'.format(result))
-        return result
+        print("Inside result: {}".format(div))
+    return (div)
