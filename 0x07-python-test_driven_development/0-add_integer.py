875e55f54a67909d24c9a21e363d8858d8663990
curtarnold <afayedormawuli@gmail.com>
2022-11-16 11:17:35 +0000

    Integers addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,24 +1,30 @@
 #!/usr/bin/python3
 
-"""Defines an integer addition function.
-Args:
-a (int): Parameter a
-b (int): Parameter b
+"""A function that performs an addition.
+
+add_integer adds a and b (cast as integers) and returns the result
+The function raises a TypeError when a or b are neither integers nor floats
 """
 
-
 def add_integer(a, b=98):
-    """Return the integer addition of a and b.
+    """Returns the result of adding two integers.
+
+    b defaults to 98 if no value is passed in.
+    a and b are cast to integers if they are floats
+
     Args:
-    a (int): Parameter a
-    b (int): Parameter b
-    Return:
-    int
+        a (int): First value to add (must be cast as an integer if a float)
+        b (int): Second value to add (must be cast as an integer if a float)
+
+    Returns:
+        An integer: the addition of a and b
+
     Raises:
-        TypeError: If either of a or b is a non-integer and non-float.
+        TypeError: if a or b are not integers or floats
     """
+
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
-    if not isinstance(b, int) and not isinstance(b, float):
+    elif not isinstance(b, int) and not isinstance(b, float):
         raise TypeError("b must be an integer")
-    return int(a) + int(b)
+    return (int(a) + int(b))
--- 0x07-python-test_driven_development/README.md
+++ 0x07-python-test_driven_development/README.md
@@ -1,2 +1,11 @@
-# TEST DRIVEN DEVELOPMENT
+# Test Driven Development
 
+In this project I have learnt the following:
+* An interactive test: This is the kind of test in which the python interactive shell is replicated in the docstring of code. This allows the `doctest` module to run the shell replicas and compare them with the outputs that are provided
+* Tests allow engineers the opportunity to check if the code they write or programs they build would run efficiently.
+* You write Docstrings to create tests by simply running code in the python interactive shell and then copying and pasting it in your docstring. Note that docstrings begin and end with tripple quotes
+* Module documentation comes as the first text before any function or class is written. The class/function documentation is written immediately after the function is declared, before any code is written
+* Some of the basic option flags used when writing tests include;
+  * doctest.ELLIPSIS
+  * doctest.NORMALIZE_WHITESPACE
+* Edge cases are found by testing the function at boundary beyond which the funtion is  not valid
