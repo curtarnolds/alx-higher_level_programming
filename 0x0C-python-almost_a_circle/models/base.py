8ede4177dbd3f38c7592b2e6693f4e4e0210eb5b
curtarnolds <afayedormawuli@gmail.com>
2023-02-15 16:34:30 +0000

    combine

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,7 @@
 """Base class to manage `id` attribute.
 """
 import json
+import csv
 
 
 class Base():
@@ -81,3 +82,24 @@ class Base():
                     f.read())]
         except IOError:
             return []
+
+
+@classmethod
+def save_to_file_csv(cls, list_objs):
+    """Write the CSV serialization of a list of objects to a file.
+
+        Args:
+            list_objs (list): A list of inherited Base instances.
+        """
+    filename = cls.__name__ + ".csv"
+    with open(filename, "w", newline="") as csvfile:
+        if list_objs is None or list_objs == []:
+            csvfile.write("[]")
+        else:
+            if cls.__name__ == "Rectangle":
+                fieldnames = ["id", "width", "height", "x", "y"]
+            else:
+                fieldnames = ["id", "size", "x", "y"]
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            for obj in list_objs:
+                writer.writerow(obj.to_dictionary())
