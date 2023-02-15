9bc48bc5c4df1a2ea36391c68d75004ff28eeba1
curtarnolds <afayedormawuli@gmail.com>
2023-02-15 15:39:27 +0000

    combine

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -27,7 +27,7 @@ class Rectangle(Base):
         """Set width"""
         if not isinstance(width, int):
             raise TypeError("width must be an integer")
-        if width < 0:
+        if width <= 0:
             raise ValueError("width must be > 0")
         self.__width = width
 
@@ -39,7 +39,7 @@ class Rectangle(Base):
     def height(self, height):
         if not isinstance(height, int):
             raise TypeError("height must be an integer")
-        if height < 0:
+        if height <= 0:
             raise ValueError("height must be > 0")
         self.__height = height
 
