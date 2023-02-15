1cf65b0f49b8fba20e87d626db0838d2b7bbbab8
curtarnolds <afayedormawuli@gmail.com>
2023-02-15 16:22:07 +0000

    combine

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -40,11 +40,12 @@ class Base():
         with open(f'{cls.__name__}.json', mode='w', encoding='utf-8')\
                 as json_file:
             if list_objs is None:
-                json_file.write([])
-            list_dict = [item.to_dictionary()
-                         for item in list_objs]  # convert each object
-            # in the list into a dictionary to avoid JSON serialize error
-            json_file.write(Base.to_json_string(list_dict))
+                json_file.write("[]")
+            else:
+                list_dict = [item.to_dictionary()
+                             for item in list_objs]  # convert each object
+                # in the list into a dictionary to avoid JSON serialize error
+                json_file.write(cls.to_json_string(list_dict))
 
     @staticmethod
     def from_json_string(json_string):
