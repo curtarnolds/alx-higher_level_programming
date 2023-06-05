e14ff79afab8ecb98eb0c0bf10ac0334fc0b5194
curtarnolds <afayedormawuli@gmail.com>
2023-06-05 20:32:19 +0000

    7. Change representation

---
+++ 0x08-python-more_classes/7-main.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+Rectangle = __import__('7-rectangle').Rectangle
+
+my_rectangle_1 = Rectangle(8, 4)
+print(my_rectangle_1)
+print("--")
+my_rectangle_1.print_symbol = "&"
+print(my_rectangle_1)
+print("--")
+
+my_rectangle_2 = Rectangle(2, 1)
+print(my_rectangle_2)
+print("--")
+Rectangle.print_symbol = "C"
+print(my_rectangle_2)
+print("--")
+
+print(my_rectangle_1)
+print(Rectangle.print_symbol)
+print(my_rectangle_1.print_symbol)
+print(my_rectangle_2.print_symbol)
+print(my_rectangle_2.__dict__)
+print(Rectangle.__dict__)
+print(my_rectangle_1.__class__.__dict__)
+print(my_rectangle_1.__dict__)
+print("--")
+
+my_rectangle_3 = Rectangle(7, 3)
+print(my_rectangle_3)
+
+print("--")
+
+my_rectangle_3.print_symbol = ["C", "is", "fun!"]
+print(my_rectangle_3)
+
+print("--")
