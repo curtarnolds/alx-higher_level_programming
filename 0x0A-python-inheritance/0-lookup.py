9a98f3d3d5c04420ef4ef09f5dc2e982ded07443
curtarnolds <afayedormawuli@gmail.com>
2023-06-12 10:55:06 +0000

    Implemented lookup() in 0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Lookup module to define a function that returns an object's attributes
+and methods
+"""
+
+
+def lookup(obj):
+    """Return a list of attributes and methods of `obj`."""
+    return dir()
