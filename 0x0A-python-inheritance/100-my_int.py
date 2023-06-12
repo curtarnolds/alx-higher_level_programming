595fa4be9ca07513345410a74f7340b3ed64a066
curtarnolds <afayedormawuli@gmail.com>
2023-06-12 23:20:51 +0000

    Swapped equality and inequality in MyInt

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -4,4 +4,11 @@
 
 class MyInt(int):
     """A class that inherits from int."""
-    pass
+
+    def __eq__(self, value: object) -> bool:
+        """Redefines equality of MyInt."""
+        return False
+
+    def __ne__(self, value):
+        """Redefines inequality of MyInt."""
+        return True
