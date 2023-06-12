bf34e1e96f2ab6f46bc8d181600761a9ffb48a55
curtarnolds <afayedormawuli@gmail.com>
2023-06-12 23:29:07 +0000

    101-add_attribute.py

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""A module to define an add_attribute function."""
+
+
+def add_attribute(obj, attr_name, attr_val):
+    """Adds a new attribute to an object.
+
+    Args:
+        attr_name (str): Attribute name
+        attr_val (str): Attribute value
+
+    Raises:
+        TypeError if object can't have new attribute
+    """
+    pass
