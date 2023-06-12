0bd2f1a034d7422fa871aa1b0ab2b7fad125a4d7
curtarnolds <afayedormawuli@gmail.com>
2023-06-12 19:02:38 +0000

    Implemented integer_validator() in 7-integer_validator.py with doctests in tests/

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module to hold an empty BaseGeometry class."""
+
+
+class BaseGeometry:
+    """An empty BaseGeometry class."""
+
+    def area(self):
+        """Raises an exception."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self,  name, value):
+        """Validates `value`."""
+        if not isinstance(name, str):
+            raise TypeError()
+        if not isinstance(value, int):
+            raise TypeError(f"<{name}> must be an integer")
+        if value <= 0:
+            raise ValueError(f"<{name}> must be greater than 0")
