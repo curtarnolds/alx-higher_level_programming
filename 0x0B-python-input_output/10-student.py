8002ad36af24f0fda4baade12d1162694947c630
curtarnolds <afayedormawuli@gmail.com>
2023-06-13 20:30:15 +0000

    control effected for to_json() in 10-main.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""A Student class."""
+
+
+class Student:
+    """Defines a student."""
+    def __init__(self, first_name, last_name, age):
+        """Initialize a student instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieve a dictionary representation of a Student instance.
+
+        Description:
+            If `attrs` is given, only attribuutes in `attrs` are retrieved.
+        """
+        if attrs and len(attrs) > 0:
+            attr_dict = {}
+            for item in attrs:
+                if hasattr(self, item):
+                    attr_dict[item] = self.__dict__[item]
+            return attr_dict
+        return self.__dict__
