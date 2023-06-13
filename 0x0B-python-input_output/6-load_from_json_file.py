fccc0eaaafc4cebafdcb01df8507ffed1c754bdf
curtarnolds <afayedormawuli@gmail.com>
2023-06-13 15:45:33 +0000

    load_from_json_file() implemented in 6-load_from_json_file.py

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/python3
+"""A function that creates an object from a JSON file."""
+
+
+def load_from_json_file(filename: str) -> object:
+    """Create an object from a JSON file
+
+    Args:
+        filename: Name of the file to write to.
+    """
+    import json
+    with open(filename, mode='r', encoding='utf-8') as json_file:
+        return json.load(json_file)
