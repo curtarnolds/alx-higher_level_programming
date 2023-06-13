0beefd1ae598ba70cb8d39cada01074054b366aa
curtarnolds <afayedormawuli@gmail.com>
2023-06-13 01:34:25 +0000

    Implemented logic on 101-add_attribute.py

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""A module to define an add_attribute function."""
+"""A module to define an add_attribute function.
+"""
 
 
 def add_attribute(obj, attr_name, attr_val):
@@ -12,8 +13,6 @@ def add_attribute(obj, attr_name, attr_val):
     Raises:
         TypeError if object can't have new attribute
     """
-    if type(attr_name) is not str:
-        return
     if not hasattr(obj, "__dict__"):
         raise TypeError("Can't add new attribute")
     setattr(obj, attr_name, attr_val)
