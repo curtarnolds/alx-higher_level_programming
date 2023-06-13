d94c2eb5bca7d19572c71f321826e10a751be033
curtarnolds <afayedormawuli@gmail.com>
2023-06-13 13:38:42 +0000

    read_file() implemented in 0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""A simple file reading function."""
+
+
+def read_file(filename=""):
+    """Read a text file.
+
+    Args:
+        filename (str): Name of file.
+    """
+    with open(filename, mode='r', encoding='utf-8') as txt_file:
+        print(txt_file.read())
