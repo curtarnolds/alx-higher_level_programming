ba7b75b925da69e844faf6bfa4476dace2003365
curtarnolds <afayedormawuli@gmail.com>
2023-06-13 01:39:24 +0000

    Implemented logic on 101-add_attribute.py

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -7,6 +7,7 @@ def add_attribute(obj, attr_name, attr_val):
     """Adds a new attribute to an object.
 
     Args:
+        obj (objec): An object
         attr_name (str): Attribute name
         attr_val (str): Attribute value
 
@@ -15,4 +16,8 @@ def add_attribute(obj, attr_name, attr_val):
     """
     if not hasattr(obj, "__dict__"):
         raise TypeError("Can't add new attribute")
+
+    if not isinstance(attr_name, str):
+        raise ValueError("Attribute name must be a string")
+
     setattr(obj, attr_name, attr_val)
