86c81c2970afeb09d74c571e506315b7ae1ec109
curtarnolds <afayedormawuli@gmail.com>
2023-07-19 13:25:01 +0000

    0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,5 @@
 -- List all privileges of the MySQL users `user_0d_1` and `user_0d_2` on `localhost`
 -- List user_0d_1 privileges
-SHOW GRANTS FOR "user_0d_1"@"localhost";
+SHOW GRANTS IF EXISTS FOR "user_0d_1"@"localhost";
+-- List user_0d_2 privileges
 SHOW GRANTS FOR "user_0d_2"@"localhost";
