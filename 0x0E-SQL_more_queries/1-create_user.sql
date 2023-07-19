985819dc3a4c184e078b0c64349c1fc14a2b665f
curtarnolds <afayedormawuli@gmail.com>
2023-07-19 13:45:04 +0000

    1. Root user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,3 @@
 -- Create the MySQL user `user_0d_1` if it doesn't exist
-CREATE USER IF NOT EXISTS user_0d_1 IDENTIFIED BY "user_0d_1_pwd"
+CREATE USER IF NOT EXISTS user_0d_1 IDENTIFIED BY "user_0d_1_pwd";
+GRANT ALL PRIVILEGES ON *.* TO "user_0d_1"@"localhost";
