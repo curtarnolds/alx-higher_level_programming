859baba19d71c3dc43acdabc6b0e644983b825cb
curtarnolds <afayedormawuli@gmail.com>
2023-08-14 21:03:30 +0000

    8. Square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,14 +1,18 @@
 #!/usr/bin/node
 
 const argument = process.argv;
-const array = [];
-let i = 0;
-while (i < Number(argument[2])) {
-  array.push('X');
-  i++;
-}
-i = 0;
-while (i < Number(argument[2])) {
-  console.log(...array);
-  i++;
+if (!isNaN(Number(argument[2]))) {
+  const array = [];
+  let i = 0;
+  while (i < argument[2]) {
+    array.push('X');
+    i++;
+  }
+  i = 0;
+  while (i < argument[2]) {
+    console.log(...array);
+    i++;
+  }
+} else {
+  console.log('Missing size');
 }
