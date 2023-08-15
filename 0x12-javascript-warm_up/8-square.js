b5d3d1fe3021f00b9bca23cb4d79961443252fdf
curtarnolds <afayedormawuli@gmail.com>
2023-08-15 04:26:39 +0000

    8. Square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,15 +2,13 @@
 
 const argument = process.argv;
 if (!isNaN(Number(argument[2]))) {
-  const array = [];
-  let i = 0;
-  while (i < argument[2]) {
-    array.push('X');
-    i++;
+  let sampleString = '';
+  for (let index = 0; index < Number(argument[2]); index++) {
+    sampleString = sampleString + 'X';
   }
-  i = 0;
-  while (i < argument[2]) {
-    console.log(...array);
+  let i = 0;
+  while (i < Number(argument[2])) {
+    console.log(sampleString);
     i++;
   }
 } else {
