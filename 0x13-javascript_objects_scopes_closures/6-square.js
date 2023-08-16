882431e4c4e956dfb8590854d9bec2c08eff744b
curtarnolds <afayedormawuli@gmail.com>
2023-08-16 19:24:02 +0000

    6. Square #1

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -7,12 +7,14 @@ module.exports = class Square extends Rectangle {
   }
 
   charPrint (c = 'X') {
-    let xRect = '';
-    for (let j = 0; j < this.height; j++) {
-      xRect = xRect + c;
-    }
-    for (let i = 0; i < this.height; i++) {
-      console.log(xRect);
+    if (this.height > 0) {
+      let xRect = '';
+      for (let j = 0; j < this.height; j++) {
+        xRect = xRect + c;
+      }
+      for (let i = 0; i < this.height; i++) {
+        console.log(xRect);
+      }
     }
   }
 };
