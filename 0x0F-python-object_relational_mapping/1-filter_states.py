64d9ef8eb7ca432029e51cc65d442349e508b7a3
curtarnolds <afayedormawuli@gmail.com>
2023-08-18 04:54:19 +0000

    1. Filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""A MySQLdb script to list all `states` from the database `hbtn_0e_0_usa`"""
+import MySQLdb
+
+
+db = MySQLdb.connect(host='localhost', port=3306, user='', passwd='',
+                     db='hbtn_0e_0_usa')
+cur = db.cursor()
+col_count = cur.execute("SELECT * FROM states ORDER BY id")
+table_rows = cur.fetchall()
+for row in table_rows[-2:]:
+    print(row)
