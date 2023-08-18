dc8b58ddd7d322fc57bf440eef6ddb09924e5fb9
curtarnolds <afayedormawuli@gmail.com>
2023-08-18 23:54:52 +0000

    5. All cities by state

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""A MySQLdb script to list all `states` from the database `hbtn_0e_0_usa`.
+Usage:
+    ./1-filter.py `user` `password` `database` `state name searched`
+"""
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    args = sys.argv
+    db = MySQLdb.connect(user=args[1], passwd=args[2], db=args[3])
+    cur = db.cursor()
+    col_count = cur.execute(f"SELECT DISTINCT(cities.name)\
+                            FROM `cities` INNER JOIN `states` ON \
+                            cities.state_id = (SELECT states.id FROM states\
+                            WHERE states.name = '{args[4].split(';')[0]}')")
+    table_rows = cur.fetchall()
+    temp_list = [name[0] for name in table_rows]
+    print(", ".join(temp_list))
+    cur.close()
+    db.close()
