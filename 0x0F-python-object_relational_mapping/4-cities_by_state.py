f25769b4ee0f50e0a1e32df8b3f5c47778bab4e8
curtarnolds <afayedormawuli@gmail.com>
2023-08-19 00:17:08 +0000

    4. Cities by states

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -11,7 +11,9 @@ if __name__ == "__main__":
     args = sys.argv
     db = MySQLdb.connect(user=args[1], passwd=args[2], db=args[3])
     cur = db.cursor()
-    col_count = cur.execute("SELECT cities.id, cities.name, states.name FROM `cities` INNER JOIN states ON cities.state_id = states.id ORDER BY cities.id")
+    col_count = cur.execute("SELECT cities.id, cities.name, states.name FROM\
+                            `cities` INNER JOIN states ON cities.state_id =\
+                            states.id ORDER BY cities.id")
     table_rows = cur.fetchall()
     for row in table_rows:
         print(row)
