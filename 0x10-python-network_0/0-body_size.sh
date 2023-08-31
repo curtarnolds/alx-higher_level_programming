b0e2e871970f4dfa9df2922d7daca99f74add76e
curtarnolds <afayedormawuli@gmail.com>
2023-08-31 19:39:56 +0000

    0. cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send a request to a URL, and display the size of the body of the response
-curl -sI "$1" | grep -i "Content-Length" | cut -d " " -f2
+curl -s -w "%{size_download}" "$1"
