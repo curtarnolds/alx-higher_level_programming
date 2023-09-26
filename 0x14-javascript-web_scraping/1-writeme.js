a44632788c701100fd652ea1d4095746a7dda152
curtarnolds <afayedormawuli@gmail.com>
2023-09-26 11:23:14 +0000

    1. Write me

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const fs = require('fs').promises;
+
+async function writeFile(filePath, str) {
+    try {
+        await fs.writeFile(filePath, str, 'utf-8');
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+writeFile(process.argv[2], process.argv[3])
