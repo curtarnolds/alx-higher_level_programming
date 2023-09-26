07e0bd6f357822639af483dd7cf08219845e2f6a
curtarnolds <afayedormawuli@gmail.com>
2023-09-26 12:16:51 +0000

    2. Status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+
+const request = require('request')
+
+const url = process.argv[2]
+const response = request(url, (error, response, body) => console.log(`code: ${response && response.statusCode}`));
