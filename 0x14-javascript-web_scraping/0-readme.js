ae9675b5fc87a9863a76efadaa9c4a2f8a6110e3
curtarnolds <afayedormawuli@gmail.com>
2023-09-26 12:34:53 +0000

    0. Readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 
-const fs = require("fs").promises;
+const fs = require('fs').promises;
 
-async function readFile(filePath) {
-	try {
-		const data = await fs.readFile(filePath, "utf-8");
-		console.log(data.toString());
-	} catch (error) {
-		console.error(error);
-	}
+async function readFile (filePath) {
+  try {
+    const data = await fs.readFile(filePath, 'utf-8');
+    console.log(data.toString());
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 readFile(process.argv[2]);
