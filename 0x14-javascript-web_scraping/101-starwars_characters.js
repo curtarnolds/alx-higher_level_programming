1b7657feb2d4894201aa102f87ae7ed996b1e5c8
curtarnolds <afayedormawuli@gmail.com>
2023-09-27 01:52:11 +0000

    8. Right order

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,21 +3,22 @@
 const request = require('request').get;
 
 const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
+
 request(url, (error, response, body) => {
   if (!error && response) {
     const characters = JSON.parse(body).characters;
-    for (let i = 0; i < characters.length; i++) {
-      const element = characters[i];
-      request(element, (err, resp, data) => {
-        if (!err && resp) {
-          console.log(JSON.parse(data).name);
-        }
-      });
-    }
-    // characters.forEach(character => request(`${character}`, (err, resp, data) => {
-    //   if (!err && resp) {
-    //     console.log(JSON.parse(data).name);
-    //   }
-    // }));
+
+    const getCharacterName = (characters, index = 0) => {
+      if (index < characters.length) {
+        const char = characters[index];
+        request(char, (err, resp, data) => {
+          if (!err && resp) {
+            console.log(JSON.parse(data).name);
+            getCharacterName(characters, index + 1);
+          }
+        });
+      }
+    };
+    getCharacterName(characters);
   }
 });
